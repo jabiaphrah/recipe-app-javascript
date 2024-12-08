9ff67c7b78f8e371191493c77942919739b2802a
mark <watetumark7@icloud.com>
2024-12-08 17:31:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let recipe = [
+    {
+         id:1,
+         recipeName:"Strawberry Cake",
+         recipeImage:"image/strawberry-cake.jpg",
+    
+         recipeIngredient: [
+            "400gram crackers",
+            "150g unsalted butter melted",
+            "300g marshmellows",
+        ],
+    
+        recipeIngredient: [
+            "To prepare crust add graham crackers to a food processor and process until you reach fine crumbs. Add melted butter and pulse 3–4 times to coat crumbs with butter",
+            "To prepare crust add graham crackers to a food processor and process until you reach fine crumbs. Add melted butter and pulse 3–4 times to coat crumbs with butter",
+            "To prepare crust add graham crackers to a food processor and process until you reach fine crumbs. Add melted butter and pulse 3–4 times to coat crumbs with butter",
+        ]
+     }, 
+    ]
+    
+    let recipeListContainer = document.querySelector("#recipe-display-container")
+    let recipeCard = document.getElementById("card");
+    
